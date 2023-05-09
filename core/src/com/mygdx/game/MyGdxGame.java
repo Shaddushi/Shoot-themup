bfcc732178fd48d7cc7f9c596cb4872e93e673ca
yoanTurpin <yoan.turpin0@gmail.com>
2023-05-09 11:01:23 +0200

    base qui marche

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,25 +2,21 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-<<<<<<< HEAD
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-=======
->>>>>>> 16bffcf92a7752ef5687778771a0c808d311c567
+
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.GL20;
 import com.mygdx.game.characters.hero.Hero;
 
 public class MyGdxGame extends ApplicationAdapter {
-<<<<<<< HEAD
 
 	public ShapeRenderer shape;
 	public Hero hero;
 	public Texture heroimg;
 	public SpriteBatch batch;
-	
+
 	@Override
 	public void create () {
 
@@ -33,32 +29,16 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			hero.gauche();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			hero.droite();
+		}
 		batch.begin();
 		hero.draw(batch);
 		batch.end();
 
-=======
-	ShapeRenderer shape;
-
-	Hero hero;
-	@Override
-	public void create () {
-		hero = new Hero(50,50,20,10);
-		shape = new ShapeRenderer();
-
-	}
-
-	@Override
-	public void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if (hero.x < Gdx.graphics.getWidth()){
-			System.out.println(hero.speed);
-			hero.x =hero.x + hero.speed;
-		}
-		shape.begin(ShapeRenderer.ShapeType.Filled);
-		shape.circle(hero.x, hero.y, 30);
-		shape.end();
->>>>>>> 16bffcf92a7752ef5687778771a0c808d311c567
 	}
 
 }
