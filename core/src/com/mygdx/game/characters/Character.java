2d7e3249493df27a22b22f50bac45ea97cee7d75
Shaddushi <duportail.honore@gmail.com>
2023-05-09 09:14:33 +0200

    move

--- core/src/com/mygdx/game/characters/Character.java
+++ core/src/com/mygdx/game/characters/Character.java
@@ -6,12 +6,38 @@ public class Character {
     int x;
     int y;
     int life;
+
     int level;
 
-    public Character(int x, int y, int xspeed, int yspeed, int life){
+    public Character(int x, int y, int xspeed, int yspeed, int life) {
         this.x = x;
         this.y = y;
         this.xspeed = xspeed;
         this.yspeed = yspeed;
         this.life = life;
+    }
+
+    public void gauche(){
+        this.x = this.x - this.xspeed;
+    }
+
+
+    public void droite(){
+        this.x = this.x + this.xspeed;
+    }
+
+
+
+    public void haut(){
+        this.y = this.y + this.yspeed;
+    }
+
+
+    public void bas(){
+        this.y = this.y - this.yspeed;
+    }
+
+
+
+
 }
---
+++ core/src/com/mygdx/game/weapon/bullet.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.weapon;
+
+public class bullet {
+
+    int degat;
+
+    int speed;
+
+    public bullet(int degat,int speed){
+        this.degat = degat;
+        this.speed = speed;
+    }
+
+    
+
+
+}
