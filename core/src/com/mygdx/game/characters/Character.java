6afb1f2b23e76b77ca18f18b9d7643597a56e9be
FloDoyer <doyer.florent@icloud.com>
2023-05-09 09:20:16 +0200

    a

--- core/src/com/mygdx/game/characters/Character.java
+++ core/src/com/mygdx/game/characters/Character.java
@@ -1,19 +1,20 @@
 package com.mygdx.game.characters;
 
-public class Character {
-    int xspeed;
-    int yspeed;
+public abstract class Character {
+    int speed;
     int x;
     int y;
     int life;
     int degat;
 
-    public Character(int x, int y, int xspeed, int yspeed, int life, int degat){
+    public Character(int x, int y, int speed, int life, int degat){
         this.x = x;
         this.y = y;
-        this.xspeed = xspeed;
-        this.yspeed = yspeed;
+        this.speed = speed;
         this.life = life;
         this.degat = degat;
     }
+
+    abstract protected void deplacement();
+
 }
