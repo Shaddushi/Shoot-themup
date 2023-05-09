95f5f4afb3ed24a9f729da1112f1a1a1a75fef2c
Shaddushi <duportail.honore@gmail.com>
2023-05-09 11:11:58 +0200

    monster

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,28 +2,34 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-<<<<<<< HEAD
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-=======
->>>>>>> 16bffcf92a7752ef5687778771a0c808d311c567
+
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.GL20;
 import com.mygdx.game.characters.hero.Hero;
+import com.mygdx.game.characters.monster.Monster;
 
 public class MyGdxGame extends ApplicationAdapter {
-<<<<<<< HEAD
 
 	public ShapeRenderer shape;
 	public Hero hero;
 	public Texture heroimg;
+
+	public Texture monsterimg;
 	public SpriteBatch batch;
-	
+
+	public Monster[] m = new Monster[4];
 	@Override
 	public void create () {
+		monsterimg = new Texture("player.png");
+		for(int i =0;i<4;i++){
 
+			m[i] = new Monster(40*i,40,20,10,10,monsterimg);
+		}
 		batch = new SpriteBatch();
 		heroimg = new Texture("player.png");
 		hero = new Hero(250, 250, 20, 100, 15, heroimg);
@@ -33,32 +39,25 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			hero.gauche();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			hero.droite();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			hero.haut();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			hero.bas();
+		}
 		batch.begin();
 		hero.draw(batch);
+		for(int i = 0;i<4;i++){
+			m[i].draw(batch);
+		}
 		batch.end();
 
-=======
-	ShapeRenderer shape;
-
-	Hero hero;
-	@Override
-	public void create () {
-		hero = new Hero(50,50,20,10);
-		shape = new ShapeRenderer();
-
-	}
-
-	@Override
-	public void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if (hero.x < Gdx.graphics.getWidth()){
-			System.out.println(hero.speed);
-			hero.x =hero.x + hero.speed;
-		}
-		shape.begin(ShapeRenderer.ShapeType.Filled);
-		shape.circle(hero.x, hero.y, 30);
-		shape.end();
->>>>>>> 16bffcf92a7752ef5687778771a0c808d311c567
 	}
 
 }
