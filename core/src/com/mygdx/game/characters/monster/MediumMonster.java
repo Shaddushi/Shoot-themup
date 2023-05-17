9b8144a6dbf91835823caeb8d287c69a091c26ae
FloDoyer <doyer.florent@icloud.com>
2023-05-17 13:38:16 +0200

    aaaaaaaaaaa

--- core/src/com/mygdx/game/characters/monster/MediumMonster.java
+++ core/src/com/mygdx/game/characters/monster/MediumMonster.java
@@ -9,36 +9,16 @@ import java.util.Random;
 
 public class MediumMonster extends Monster {
 
-    private int x;
-    private int y;
     public Random random = new Random();
     private int rand;
 
     public MediumMonster(int x, int y){
-    super(x,y,20, -3, 1, new Texture("mediumMonster.png"), 2 , 10);
-    //this.rand = random.nextInt(10);
+    super(x,y,5,3 , 1, new Texture("mediumMonster.png"), 2 , 100);
+    this.rand = random.nextInt(20);
+
     }
 
-    public void update(){
-        this.x += xspeed;
-        this.y +=yspeed;
-        if (this.x <= 0){
-            this.x = 0;
-        }
-        if(this.x >= Gdx.graphics.getWidth() - this.getTaillex()){
-            this.x = Gdx.graphics.getWidth() - this.getTaillex();
-        }
-        if(this.y <= 0){
-            this.yspeed = 0;
-        }
-        /*if(rand <= 0){
-            this.x = -this.x;
-            rand = random.nextInt(10);
-        }
-        else{
-            rand--;
-        }*/
-    }
+
 
     public Bullet tirer() {
         Bullet BE =new BulletEnnemi(this.x,this.y);
@@ -46,6 +26,26 @@ public class MediumMonster extends Monster {
         return BE;
 
 
+    }
+    public void update(){
+        this.x += this.xspeed;
+        this.y =this.y - this.yspeed;
+        if (this.x <= 0){
+            this.x = 0;
+        }
+        if(this.x >= Gdx.graphics.getWidth() - this.getTaillex()){
+            this.x = Gdx.graphics.getWidth() - this.getTaillex();
+        }
+        if(this.y < (int)(Gdx.graphics.getHeight()-Gdx.graphics.getHeight()*0.6) || y > Gdx.graphics.getHeight() - this.getTailley()){
+            yspeed = -yspeed;
+        }
+        if(rand <= 0){
+            this.xspeed = -this.xspeed;
+            rand = random.nextInt(10);
+        }
+        else{
+            rand--;
+        }
     }
 
 
