c1e7a9fcaa3796b4a75ebe9ba3bf7ebfde64ffc0
yoanTurpin <yoan.turpin0@gmail.com>
2023-05-17 16:02:15 +0200

    C'est fait

--- core/src/com/mygdx/game/weapon/Bullet.java
+++ core/src/com/mygdx/game/weapon/Bullet.java
@@ -52,5 +52,6 @@ public class Bullet {
         if (this.xspeed < 0) this.x = this.x - -this.xspeed ; //bullet va a gauche
         if (this.yspeed > 0) this.y = this.y + this.yspeed ; //bullet va en hau
         if (this.yspeed < 0) this.y = this.y - -this.yspeed ; //bullet va en bas
+
     }
 }
