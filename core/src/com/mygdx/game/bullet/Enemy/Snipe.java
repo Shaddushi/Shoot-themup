463a5a1b9c7478cbe894b1fe1b7a217594b23de0
FloDoyer <doyer.florent@icloud.com>
2023-05-22 15:15:52 +0200

    jvnds<wxh

--- core/src/com/mygdx/game/bullet/Enemy/Snipe.java
+++ core/src/com/mygdx/game/bullet/Enemy/Snipe.java
@@ -1,4 +1,14 @@
 package com.mygdx.game.bullet.Enemy;
 
-public class Snipe {
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class Snipe extends BulletEnnemi {
+    public snipe(int x, int y, int xspeed, int yspeed, int sens, MyGdxGame gdx){
+        super(2,x,y,xspeed,yspeed,new Texture("bulletBoss1.png"), 50);
+        this.xdep = x;
+        this.sens = sens;
+        this.gdx = gdx;
+    }
+
 }
